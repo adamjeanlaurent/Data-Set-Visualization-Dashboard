7fd0311aab568671e872b94c36a8f3fc1778b967
Adam Jean-Laurent <adamjeanlaurent@gmail.com>
2020-11-24 11:13:26 -0500

    thin kabout what  i want to display

---
+++ scripts/grapher.js
@@ -0,0 +1,15 @@
+/**
+ * @summary: Has method for creating graphs.
+ */
+
+class Grapher {
+    constructor(df) {
+        this.df = df;
+    }
+    barPlot() {
+        // get index
+        
+    }
+}
+
+export default Grapher;
